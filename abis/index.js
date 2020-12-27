58a4ea6e9a8205e7c189dc924357b4a1de3152e7
zach-hijazi <zach.hijazi@gmail.com>
2020-12-27 11:11:38 -0800

    Adding updates for honeyswap and adding information for balancer exchange

--- abis/index.js
+++ abis/index.js
@@ -1,7 +1,11 @@
 const kyber = require('./kyber.json');
 const uni = require('./uni.json');
+const balancer = require('./balancer.json');
+const honey = require('./honey.json');
 
 module.exports = {
   kyber,
-  uni
+  uni,
+  balancer,
+  honey
 };
